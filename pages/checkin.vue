1e052e307f297701f1f3e234559e2105092d8ae4
Juliusbecker <julix.becker@gmail.com>
2024-02-02 13:38:00 +0100

    Add: Tailwind

---
+++ pages/checkin.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="font-bold">XU University Check-In</h1>
+        <p>Check-in to your classes here</p>
+        <button @click="checkIn">Check-In</button>
+    </div>
+</template>
+
+<script setup>
+function checkIn() {
+    alert('You have checked in to your class');
+}
+</script>
